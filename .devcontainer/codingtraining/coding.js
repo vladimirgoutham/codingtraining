f65929f397ce8c4a3a5df304bff16e2e1a80e6a2
vladimirgoutham <vladimirgouthamshelby@gmail.com>
2025-02-07 12:45:57 +0000

    changes saved successfully

---
+++ .devcontainer/codingtraining/coding.js
@@ -0,0 +1,65 @@
+// let someNumber = 40
+// let stringNumber= String(someNumber)
+
+// console.log(stringNumber);
+
+// let value = 10
+// let negValue = -value
+// console.log(negValue);
+
+//console.log(2*2);
+//console.log(234**2);
+//console.log(432+456);
+//console.log(3423%23432);
+
+// let str1= "hello "
+// let str2=" midhilaj"
+// let str3= str1+str2
+// console.log(str3);
+// console.log((5+4)+(5-1));
+// console.log(+"");
+// let mum1,num2,num3
+// num1=num2=num3= 3+5
+// let gameCounter=100;
+//  gameCounter++;
+// console.log(gameCounter);
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
+
+// console.log(2>5);
+// console.log(2<5);
+// console.log(2!=5);
+// console.log(2>=5);
+// console.log(2==5);
+// console.log(2<=5);
+
+// js comparisons .verry easy than actually expected
+console.log("2">1);
+console.log("2"===1);
+
+// === this is called strict operartor.it not only compare the data but also compare the data type
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
